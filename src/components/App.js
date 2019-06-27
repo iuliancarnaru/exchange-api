9eb5b0cf0c0105b819cf3f77a2af53b5e3b7aeee
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-27 18:53:45 +0100

    basic setup

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <MainWrapper>
+      <h1>Exchange</h1>
+    </MainWrapper>
+  );
+}
+
+export default App;
+
+const MainWrapper = styled.div`
+  max-width: 1140px;
+  margin: 0 auto;
+  font-family: "PT Sans", sans-serif;
+  box-sizing: border-box;
+`;
