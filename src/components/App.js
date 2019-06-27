7790b183045ba6363e0e184b95757d4df2125524
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-27 19:46:03 +0100

    palying wirh axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({ loading: true, data: null });
+
+  useEffect(() => {
+    axios
+      .get(`https://api.exchangeratesapi.io/latest`)
+      .then(response => setData({ loading: false, data: response.data }));
+  }, []);
+
   return (
     <MainWrapper>
-      <h1>Exchange</h1>
+      <h1>Foreign exchange rates</h1>
+      {data.loading ? "Loading..." : <code>{JSON.stringify(data)}</code>}
     </MainWrapper>
   );
 }
@@ -17,3 +27,5 @@ const MainWrapper = styled.div`
   font-family: "PT Sans", sans-serif;
   box-sizing: border-box;
 `;
+
+//https://api.exchangeratesapi.io/latest
